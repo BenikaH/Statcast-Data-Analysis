ef53f9a4e915e5fb75a1ab9dd09351384e590295
Jacob Howenstein <jacob.howenstein@gmail.com>
2017-07-29 15:00:34 -0500

    general updates

--- statcast_analysis_main.py
+++ statcast_analysis_main.py
@@ -7,8 +7,8 @@ from pitching_data_functions import *
 
 # Main analysis script to go here
 
-pitcher_name = 'Wainwright'
-pitcher_data_file = 'wainwright_2017.csv'
+pitcher_name = 'Martinez'
+pitcher_data_file = 'martinez_2017.csv'
 
 pitcher1 = Pitcher(pitcher_name, pitcher_data_file)
 pitcher1.makeGames()
